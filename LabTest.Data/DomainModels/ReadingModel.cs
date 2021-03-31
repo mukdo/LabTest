8239858e4b58a0ddf20ac079ed7d3d9a57fe647c
Mukdo <bmukdo@gmail.com>
2021-03-31 22:48:42 +0600

    change random value to database

---
+++ LabTest.Data/DomainModels/ReadingModel.cs
@@ -0,0 +1,16 @@
+﻿using LabTest.Data.Models;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace LabTest.Data.DomainModels
+{
+    [NotMapped]
+    public class ReadingModel : Reading
+    {
+
+        public DateTime TimestampFrom { get; set; }
+        public DateTime TimestampTo { get; set; }
+
+    }
+}
---
+++ LabTest.Data/LabTestContext.cs
@@ -0,0 +1,47 @@
+﻿using LabTest.Data.Models;
+using LabTest.Data.ViewModels;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LabTest.Data
+{
+    public class LabTestContext : DbContext
+    {
+        public LabTestContext(DbContextOptions<LabTestContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+        }
+
+        #region Tables
+       
+
+        public  DbSet<Building> Buildings { get; set; }
+        public DbSet<DataField> DataFields { get; set; }
+        public DbSet<MyObject> MyObjects { get; set; }
+        public DbSet<Reading> Readings { get; set; }
+
+
+
+        #endregion
+
+        #region Views
+
+      
+        
+
+
+        #endregion
+    }
+}
